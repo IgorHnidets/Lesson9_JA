07ca239c92e0e64315696c02d269322b579af824
IgorHnidets <igora6669@gmail.com>
2022-09-26 18:30:05 +0300

    commit1

--- src/main/java/JDBC/filter/AdminFilter.java
+++ src/main/java/JDBC/filter/AdminFilter.java
@@ -1,12 +1,14 @@
 package JDBC.filter;
 
-import JDBC.entiti.Role;
+
 import JDBC.models.SessionParams;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+
 @WebFilter(value = {"/createProduct.jsp"})
 public class AdminFilter implements Filter {
     @Override
